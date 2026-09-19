1e7eb87da48573ef70c21fa5cce34d3ffa59b416
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-09-20 08:49:09 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod hot_lib {
 }
 
 fn model(app: &nannou::App) -> Model {
-    Model::for_window(app.new_window().view(view).build().unwrap())
+    Model::for_window(app.new_window().view(view).build())
 }
 
 pub fn update(app: &App, model: &mut Model) {
